0b75594e7bffb945dbcc27a90a226a4d54890a10
rozek00 <148904436+rozek00@users.noreply.github.com>
2025-02-27 18:40:01 +0100

    Create Palindrome_Number.cs

---
+++ Palindrome_Number.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(int x) 
+    {
+        int i=0;
+        string name = x.ToString();
+        int j = name.Length-1;
+        while(i<j)
+        {
+            if(name[i]!=name[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}

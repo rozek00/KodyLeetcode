774061283c856a4d615425af1c13da6827057989
rozek00 <148904436+rozek00@users.noreply.github.com>
2025-02-27 18:43:29 +0100

    Update Jump_Game.cs

--- Jump_Game.cs
+++ Jump_Game.cs
@@ -1,3 +1,6 @@
+//You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+//Return true if you can reach the last index, or false otherwise.
+
 public class Solution {
     public bool CanJump(int[] nums) 
     {

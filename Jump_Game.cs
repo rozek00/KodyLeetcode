96c46096f58972ef769608651b103b7f38282797
rozek00 <148904436+rozek00@users.noreply.github.com>
2025-02-27 18:31:47 +0100

    Update Jump_Game.cs

--- Jump_Game.cs
+++ Jump_Game.cs
@@ -1,1 +1,30 @@
-
+public class Solution {
+    public bool CanJump(int[] nums) 
+    {
+        int i;
+        int maxindex=0;
+        if(nums.Length==1)
+        {
+            return true;
+        }
+        for(i=0;i<nums.Length;i++)
+        {
+            if(nums[i]==0)
+            {
+                if(maxindex==nums.Length-1)
+                {
+                    return true;
+                }
+                if(maxindex<=i)
+                {
+                    return false;
+                }
+            }
+            if(i+nums[i]>maxindex)
+            {
+                maxindex =i+nums[i];
+            }
+        }      
+        return true;
+    }
+}
